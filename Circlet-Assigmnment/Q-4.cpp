97f5904c64f32a1805fb7444f7a7d283c8c32efc
krisha2551 <krishajasani62@gmail.com>
2025-04-03 13:00:18 +0530

    Add files via upload

--- Circlet-Assigmnment/Q-4.cpp
+++ Circlet-Assigmnment/Q-4.cpp
@@ -8,16 +8,24 @@ int main() {
     cout << "Enter number of row: ";
     cin >> n;
 
-    for (int i = 0; i < n; i++) 
+    for (int i = 1; i <= n; i++) 
     { 
-        for (int s = 0; s < i; s++) 
+        for (int s = 1; s <= i; s++) 
         { 
             cout << "  "; 
         }
-        for (int j = 0; j < (n - i); j++) 
+
+         for (int s = 1; s <= n-i; s++) 
         { 
-            cout << (j % 2) << " "; 
+            if(s % 2==0){
+                cout << s%2 << " ";
+            
+            }
+            else{
+                cout << s % 2 << " ";
+            }
         }
+        
         cout << endl;
     }
 
