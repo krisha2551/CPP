2f4eb64c006e939bad78eb1d953b8391a3b85c76
krisha2551 <krishajasani62@gmail.com>
2025-05-26 11:03:31 +0530

    Add files via upload

--- 45-Linear Search/Binary.cpp
+++ 45-Linear Search/Binary.cpp
@@ -67,3 +67,37 @@ int binary_search(int arr[],int low, int high, int search)
 
     return 0;
  }
+
+ 
+
+
+
+// int binarySearch(int arr[], int x, int low, int high) {
+  
+
+//   while (low <= high) {
+//     int mid = low + (high - low) / 2;
+
+//     if (x == arr[mid])
+//       return mid;
+
+//     if (x > arr[mid])
+//       low = mid + 1;
+
+//     else
+//       high = mid - 1;
+//   }
+
+//   return -1;
+// }
+
+// int main(void) {
+//   int arr[] = {3, 4, 5, 6, 7, 8, 9};
+//   int x = 4;
+//   int n = sizeof(arr) / sizeof(arr[0]);
+//   int result = binarySearch(arr, x, 0, n - 1);
+//   if (result == -1)
+//     printf("Not found");
+//   else
+//     printf("Element is found at index %d", result);
+// }
